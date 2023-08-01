6bafb5c7038fbd55e65ad9c4fb4e3ba39e162819
Sarah Al-khaled <138221726+SarahAlkhaled@users.noreply.github.com>
2023-08-01 17:34:10 +0300

    Update main (2).py
    documantation and big O

--- main (2).py
+++ main (2).py
@@ -1,20 +1,32 @@
 from datetime import datetime
-
-def login():#function to get user info and login      
+#function to get usee's username and password.
+#O(1)the loop runs for a constant number of iterations(5)
+def login():
     print("Welcome to the Login Page!")
-    username = input("Please enter a username: ").lower()
-    password = input("Please enter a password: ")
+  
+    max_attempts = 5
+    attempts = 0
 
-    if username == "admin" and password.lower() == "admin123123":
-        return "Admin"
-    elif password == "":
-        return "User"
-    else:
-        return None
-      
+    while attempts < max_attempts:
+        username = input("Please enter a username: ").lower()
+        password = input("Please enter a password: ")
+
+        if username == "admin" and password.lower() == "admin123123":
+            return "Admin"
+        elif password == "":
+            return "User"
+        else:
+            attempts += 1
+            print("Incorrect Username and/or Password. Remaining attempts:", max_attempts - attempts)
+
+    print("Maximum login attempts reached. Exiting the program.")
+    return None
    #to check if the admin entered empty password, hence its a normal user
+
  ##########
-def get_max_ticket_info(ticket_list):#func to find the maximum ticket number and the highest priority among the tickets in the provided ticket_list and called In the displayStatistics().
+#func to find the maximum ticket number and the highest priority among the tickets in the provided ticket_list and called In the displayStatistics().
+#O(n),n being the number of tickets in the ticket_list.
+def get_max_ticket_info(ticket_list):
     max_ticket_number = 0
     max_priority = 0
 
@@ -26,7 +38,9 @@ def get_max_ticket_info(ticket_list):#func to find the maximum ticket number and
 
     return max_ticket_number, max_priority     
 ##########      
-def displayStatistics(ticket_list):# function responsible for displaying statistical information about the tickets stored in the ticket_list. 
+  # function responsible for displaying statistical information about the tickets stored in the ticket_list.
+#O(n), n being the len of the lis
+def displayStatistics(ticket_list):
     max_ticket_number, max_priority = get_max_ticket_info(ticket_list)
     event_tickets_count = {}  # Dictionary to store event ID and its ticket count
 
@@ -47,32 +61,44 @@ def displayStatistics(ticket_list):# function responsible for displaying statist
             print("No tickets found.")
     else:
         print("No tickets found.")
+#######
+#O(1),constant time
+# Check if the event_date is greater than or equal to today's date
 def futureEvents(event_date):
-    # Check if the event_date is greater than or equal to today's date
     today = datetime.now().strftime("%Y%m%d")
     return event_date >= today  
-########## 
+#########################
+def selection_sort(tickets):
+    n = len(tickets)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if tickets[j][3] < tickets[min_idx][3]:
+                min_idx = j
+            elif tickets[j][3] == tickets[min_idx][3] and tickets[j][1] < tickets[min_idx][1]:
+                min_idx = j
+        tickets[i], tickets[min_idx] = tickets[min_idx], tickets[i]
+###############################3
 def displayAllTickets(ticket_list):
     if not ticket_list:
         print("No tickets found.")
     else:
         print("All Tickets: ")
-        today = datetime.now().strftime("%Y%m%d")
-        #gives today's date in the format "YYYYMMDD" for easy comparison with the ticket's date_of_event.
-       # the resource of lambda function: https://sparkbyexamples.com/python/sort-using-lambda-in-python/#:~:text=A%20lambda%20function%20is%20a,to%20execute%20an%20anonymous%20function.
-        # Filter and sort the tickets based on event date and event ID
-        sorted_tickets = sorted(ticket_list, key=lambda x: (futureEvents(x[3]), x[3], x[1]))
-        for ticket in sorted_tickets:
+        today = datetime.now().strftime("%Y%m%d")#gives today's date in the format "YYYYMMDD" for easy comparison with the ticket's date_of_event.
+        today_tickets = [ticket for ticket in ticket_list if ticket[3] >= today]
+        selection_sort(today_tickets)
+
+        for ticket in today_tickets:
             ticket_id, event_id, username, date_of_event, priority = ticket
-            if futureEvents(date_of_event):
-                print("Ticket ID: " + ticket_id)
-                print("Event ID: " + event_id)
-                print("Username: " + username)
-                print("Date of Event: " + date_of_event)
-                print("Priority: " + priority)
-                print("-" * 25)
+            print("Ticket ID: " + str(ticket_id))
+            print("Event ID: " + str(event_id))
+            print("Username: " + username)
+            print("Date of Event: " + date_of_event)
+            print("Priority: " + str(priority))  # Convert priority to a string
+            print("-" * 25)
 ########## 
-def changeTicketsPriority(ticket_list):#allows the admin to change the priority of a ticket in the ticket_list
+#allows the admin to change the priority of a ticket in the ticket_list
+def changeTicketsPriority(ticket_list):#O(n), n being the number of tickets in the ticket_list
     ticket_id_to_change = input("Enter the ticket ID of the ticket you want to change priority for: ")
     new_priority = input("Enter the new priority for the changed ticket: ")
 
@@ -91,10 +117,9 @@ def changeTicketsPriority(ticket_list):#allows the admin to change the priority 
         print("Priority updated successfully!")
     else:
         print("Ticket not found! Please enter a valid ticket ID.")
-
-
 ########## 
-def disableTicket(ticket_list):#allows the admin to remove a ticket from the ticket_list
+#allows the admin to remove a ticket from the ticket_list
+def disableTicket(ticket_list):#O(n), n being the len of he list
     ticket_id_to_remove = input("Enter the ticket ID of the ticket you want to remove: ")
 
     found_ticket = False
@@ -111,9 +136,9 @@ def disableTicket(ticket_list):#allows the admin to remove a ticket from the tic
     else:
         print("Ticket not found. Please enter a valid ticket ID.")
 ########## 
-def runEvents(ticket_list):#to display and process events that are scheduled for the current day
+  #to display and process events that are scheduled for the current day
+def runEvents(ticket_list):#O(nlog n)n being the number of events for today
     today = datetime.now().strftime("%Y%m%d")
-
     today_events = [ticket for ticket in ticket_list if ticket[3] == today]
 
     if not today_events:
@@ -136,41 +161,51 @@ def runEvents(ticket_list):#to display and process events that are scheduled for
 
 
 ########## 
-def saveTickets(ticket_list, tickets_txt):#function to save the tickets in the txt file
+#function to save the tickets in the txt file
+def saveTickets(ticket_list, tickets_txt):#O(n),n being the number of tickets in the ticket_list.
     with open(tickets_txt, 'w') as file:
         for ticket in ticket_list:
             ticket_id, event_id, username, event_datetime, priority = ticket
             formatted_event_id = "ev" + str(event_id).zfill(3)
             file.write(ticket_id + "," + formatted_event_id + "," + username + "," + event_datetime + "," + str(priority) + "\n")
 ########## 
-def bookATicket(ticket_list):#allow a user (admin or regular user) to book a new ticket for an event
+ #allow a user (admin or regular user) to book a new ticket for an event
+def bookATicket(ticket_list):#O(1),constant time
     max_ticket_number, max_priority = get_max_ticket_info(ticket_list)
     username = input("Enter your username: ")
     event_id = input("Enter the ID of the event: ")
     date_of_event = input("Enter the date of the event (YYYYMMDD): ")
 
     priority = max_priority + 1
-    new_ticket_id = "tick" + str(max_ticket_number + 1).zfill(3)
-    formatted_event_id = event_id.zfill(3)  # Keep it as a simple three-digit number
+    new_ticket_id = "tick" + str(max_ticket_number + 1).zfill(3)#3 digit number
+    formatted_event_id = "ev" + event_id
 
-    # Add the 'ev' prefix to the event_id when appending the ticket
-    ticket_list.append((new_ticket_id, 'ev' + formatted_event_id, username, date_of_event, priority))
-
+    # Add the formatted event ID when appending the ticket
+    ticket_list.append((new_ticket_id, formatted_event_id, username, date_of_event, priority))
     # Save the changes to the file after adding the new ticket to the ticket_list
     saveTickets(ticket_list, "tickets_txt")
     print("Ticket booked successfully!")
-
 ##########
 # Function to display the admin menu and handle admin actions
-def displayAdminMenu():
-  print("This is the Admin Menu, choose what do you want to do:\nAdmin Menu: \n1. Display Statistics\n2. Book a Ticket \n3. Display all Tickets  \n4. Change Ticket’s Priority  \n5. Disable Ticket  \n6. Run Events\n7. Exit")
+def displayAdminMenu():#O(1),constant time
+    print("This is the Admin Menu, choose what do you want to do:")
+    print("Admin Menu: ")
+    print("1. Display Statistics")
+    print("2. Book a Ticket")
+    print("3. Display all Tickets")
+    print("4. Change Ticket’s Priority")
+    print("5. Disable Ticket")
+    print("6. Run Events")
+    print("7. Exit")
 ##########
 # Function to display the user menu and handle admin actions
-def displayUserMenu():
-  print("This is the User Menu, choose what do you want to do:\nUser Menu:  \n1. Book a ticket\n2. Exit")
-
+def displayUserMenu():#O(1),constant time
+    print("This is the User Menu, choose what do you want to do:")
+    print("User Menu:")
+    print("1. Book a ticket")
+    print("2. Exit")
 ########## 
-def main():
+def main():#O(n log n), n being the lenght conten of the list
     name = input("Enter your name, please: ")
     print("Welcome here, " + name + "!")
     
@@ -223,55 +258,4 @@ def main():
     else:
         print("Incorrect Username and/or Password or invalid user type. Exiting the program...")
 main()
-    name = input("Enter your name, please: ")
-    print("Welcome here, " + name + "!")
-    
-    # Create an empty list to hold the tickets
-    ticket_list = []
 
-    # Load tickets from the text file into the list
-    with open("tickets_txt", 'r') as file:
-        for line in file:
-            ticket_data = line.strip().split(',')
-            ticket_list.append(tuple(ticket_data))
-
-    user_type = input("Enter whether you are an admin or a user: ").lower()
-    user_role = login()
-
-    if user_role == "Admin" and user_type == "admin":
-        # Admin menu loop
-        while True:
-            displayAdminMenu()
-            choice = input("Enter the number corresponding to the operation you want to perform (1-7): ")
-            if choice == "1":
-                displayStatistics(ticket_list)
-            elif choice == "2":
-                bookATicket(ticket_list)
-            elif choice == "3":
-                displayAllTickets(ticket_list)
-            elif choice == "4":
-                changeTicketsPriority(ticket_list)
-            elif choice == "5":
-                disableTicket(ticket_list)
-            elif choice == "6":
-                runEvents(ticket_list)
-            elif choice == "7":
-                print("Exiting...")
-                break
-            else:
-                print("Invalid input. Please try again.")
-    elif user_role == "User" and user_type == "user":
-        # User menu loop
-        while True:
-            displayUserMenu()
-            choice = input("Enter the number corresponding to the operation you want to perform (1-2): ")
-            if choice == "1":
-                bookATicket(ticket_list)
-            elif choice == "2":
-                print("Exiting the program...")
-                break
-            else:
-                print("Invalid input. Please try again.")
-    else:
-        print("Incorrect Username and/or Password or invalid user type. Exiting the program...")
-main()
